aa1a4798970fe56dcb0a89148d196450d1935f01
Ryan J. Yoder <ryan@iknite.studio>
2025-06-06 14:22:22 +0100

    Add link to source code

--- app/src/App.js
+++ app/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Iknite-Space/c4-project-boilerplate"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project Source Code
         </a>
       </header>
     </div>
